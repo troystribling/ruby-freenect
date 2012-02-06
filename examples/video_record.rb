f020e24e8e463123557906e617f774eaf97fc41f
Troy Stribling <troy.stribling@gmail.com>
2012-02-06 10:50:11 -0500

    can now specify video format for record

--- examples/video_record.rb
+++ examples/video_record.rb
@@ -1,12 +1,15 @@
 $: << File.expand_path(File.join(File.dirname(__FILE__), "../lib")) << File.expand_path(File.dirname(__FILE__))
 require 'rubygems'
 require 'freenect'
+require 'tools'
 
 puts "#{Freenect.get_device_count} Kinect found"
-video_mode = Freenect.find_video_mode(:freenect_video_rgb)
-file = File.open(ARGV.first, 'wb')
 recording = true
 puts "STARTING RECORDING TO FILE: #{ARGV.first}"
+video_format = (fmt = ARGV[1]).nil? ? :freenect_video_rgb : fmt.to_sym
+video_mode = Freenect.find_video_mode(video_format)
+print_video_mode(video_mode)
+file = File.open(ARGV.first, 'wb')
 
 trap('INT') do
   puts "STOPPING"
